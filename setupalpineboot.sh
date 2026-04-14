dc0131f41275ddf0a7d7bd112ae0bd4f550af4ce
codeplugs <azharjdsjeo@gmail.com>
2026-04-14 10:19:10 +0000

    commit

--- setupalpineboot.sh
+++ setupalpineboot.sh
@@ -110,7 +110,7 @@ set timeout -1
 set qemukey [exec cat ./qemukey.pub]
 
 spawn qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
-  -drive if=pflash,format=raw,read-only=on,file=/data/data/com.termux/files/usr/share/qemu/edk2-x86_64-code.fd \
+  -drive if=pflash,format=raw,read-only=on,file=/usr/share/qemu/edk2-x86_64-code.fd \
   -drive file=alpine.img,format=raw,if=virtio \
   -netdev user,id=n1,hostfwd=tcp::2222-:22,net=192.168.50.0/24 \
   -device virtio-net,netdev=n1 \
